cd455e6bead4e8e0fd7243c7fb424b5db386b709
Andrew Kennedy <andrew-kennedy@users.noreply.github.com>
2023-04-19 17:56:37 -0700

    Add initial support for CDI devices
    Signed-off-by: Andrew Kennedy <andrew-kennedy@users.noreply.github.com>

--- pkg/parse/parse_unix.go
+++ pkg/parse/parse_unix.go
@@ -9,10 +9,55 @@ import (
 	"path/filepath"
 
 	"github.com/containers/buildah/define"
+	"github.com/container-orchestrated-devices/container-device-interface/pkg"
 	"github.com/opencontainers/runc/libcontainer/devices"
 )
 
+//go:build linux || darwin
+// +build linux darwin
+
+package parse
+
+func DeviceFromCDI(device string) (define.ContainerDevices, error) {
+	var devs define.ContainerDevices
+
+	cdiClient, err := cdi.NewClient()
+	if err != nil {
+		return nil, fmt.Errorf("creating CDI client: %w", err)
+	}
+	defer cdiClient.Close()
+
+	cdiDevices, err := cdiClient.GetDevice(device)
+	if err != nil {
+		return nil, fmt.Errorf("getting CDI device %s: %w", device, err)
+	}
+
+	for _, cdiDevice := range cdiDevices {
+		dev := define.BuildahDevice{
+			Device:       devices.Device{
+					Type: cdiDevice.Type, 
+					Major: cdiDevice.Major, 
+					Minor: cdiDevice.Minor, 
+					FileMode: os.FileMode(cdiDevice.FileMode), 
+					Uid: cdiDevice.UID, 
+					Gid: cdiDevice.GID},
+			Source:       cdiDevice.HostPath,
+			Destination:  cdiDevice.ContainerPath,
+			Permissions:  cdiDevice.Permissions,
+		}
+		devs = append(devs, dev)
+	}
+
+	return devs, nil
+}
+
 func DeviceFromPath(device string) (define.ContainerDevices, error) {
+	// Check for a CDI device
+	cdiDevs, err := DeviceFromCDI(device)
+	if err == nil {
+		return cdiDevs, nil
+	}
+	
 	var devs define.ContainerDevices
 	src, dst, permissions, err := Device(device)
 	if err != nil {
